cfbdb325b83f22979ab2e90d872374bc3184160a
mozed <32892272+mozed@users.noreply.github.com>
2017-10-18 10:02:07 +0300

    Update app.py

--- app.py
+++ app.py
@@ -9,6 +9,19 @@ import sys
 # routes contains the HTTP handlers for our server and must be imported.
 import routes
 
+class SSLWSGIRefServer(bottle.ServerAdapter):
+    def run(self, handler):
+        from wsgiref.simple_server import make_server, WSGIRequestHandler
+        import ssl
+        if self.quiet:
+            class QuietHandler(WSGIRequestHandler):
+                def log_request(*args, **kw): pass
+            self.options['handler_class'] = QuietHandler
+        srv = make_server(self.host, self.port, handler, **self.options)
+        srv.socket = ssl.wrap_socket (srv.socket,
+        certfile='server.pem', server_side=True)
+        srv.serve_forever()
+
 if '--debug' in sys.argv[1:] or 'SERVER_DEBUG' in os.environ:
     # Debug mode will enable more verbose output in the console window.
     # It must be set at the beginning of the script.
