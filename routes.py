0ccf9dbbec3ea120dfe279adf1baf9d87ad33f76
mozed <32892272+mozed@users.noreply.github.com>
2017-10-18 16:21:24 +0300

    Update routes.py

--- routes.py
+++ routes.py
@@ -42,3 +42,17 @@ def setWebhook():
     bot = telebot.Bot(TOKEN)
     botWebhookResult = bot.setWebhook(webhook_url='https://{}.azurewebsites.net/botHook'.format(APPNAME))
     return str(botWebhookResult)
+@route('/botHook', method='POST')
+def botHook():
+    bot = telebot.Bot(TOKEN)
+    update = telebot.update.Update.de_json(request.json, bot)
+    bot.sendMessage(chat_id=update.message.chat_id, text=getSum(update.message.text, update.message.from_user.username))
+    return 'OK'
+def getSum(query, userName):
+    try:
+        splittedBySum = query.split('+')
+        if len(splittedBySum) != 2:
+            raise ValueError('Too complicated stuff')
+        return str(int(splittedBySum[0]) + int(splittedBySum[1]))
+    except:
+        return  "I'm sorry, {}. I'm afraid I can't do that".format(userName)
