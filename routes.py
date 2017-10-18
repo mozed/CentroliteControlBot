13c606735056f8180d51988223592a570aabdf82
mozed <32892272+mozed@users.noreply.github.com>
2017-10-18 16:10:52 +0300

    Update routes.py

--- routes.py
+++ routes.py
@@ -4,6 +4,34 @@ import telebot
 TOKEN = 'AAFvOi0o7SbiuNrNk-T4rWD6McEtyQVUixQ'
 APPNAME = 'CentCTB'
 
+@route('/')
+@route('/home')
+@view('index')
+def home():
+    """Renders the home page."""
+    return dict(
+        year=datetime.now().year
+    )
+
+@route('/contact')
+@view('contact')
+def contact():
+    """Renders the contact page."""
+    return dict(
+        title='Contact',
+        message='Your contact page.',
+        year=datetime.now().year
+    )
+
+@route('/about')
+@view('about')
+def about():
+    """Renders the about page."""
+    return dict(
+        title='About',
+        message='Your application description page.',
+        year=datetime.now().year
+    )
 @route('/setWebhook')
 def setWebhook():
     bot = telebot.Bot(TOKEN)
