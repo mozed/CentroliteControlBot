d702429016b9c1d8f48172043058b53e627e570b
mozed <32892272+mozed@users.noreply.github.com>
2017-10-19 08:37:34 +0300

    Update routes.py

--- routes.py
+++ routes.py
@@ -9,43 +9,15 @@ import telebot
 TOKEN = '376314991:AAFvOi0o7SbiuNrNk-T4rWD6McEtyQVUixQ'
 APPNAME = 'centctb'
 
-@route('/')
-@route('/home')
-@view('index')
-def home():
-    """Renders the home page."""
-    return dict(
-        year=datetime.now().year
-    )
-
-@route('/contact')
-@view('contact')
-def contact():
-    """Renders the contact page."""
-    return dict(
-        title='Contact',
-        message='Your contact page.',
-        year=datetime.now().year
-    )
-
-@route('/about')
-@view('about')
-def about():
-    """Renders the about page."""
-    return dict(
-        title='About',
-        message='Your application description page.',
-        year=datetime.now().year
-    )
 @route('/setWebhook')
 def setWebhook():
-    bot = telebot.Bot(TOKEN)
+    bot = telegram.Bot(TOKEN)
     botWebhookResult = bot.setWebhook(webhook_url='https://{}.azurewebsites.net/botHook'.format(APPNAME))
     return str(botWebhookResult)
 @route('/botHook', method='POST')
 def botHook():
-    bot = telebot.Bot(TOKEN)
-    update = telebot.update.Update.de_json(request.json, bot)
+    bot = telegram.Bot(TOKEN)
+    update = telegram.update.Update.de_json(request.json, bot)
     bot.sendMessage(chat_id=update.message.chat_id, text=getSum(update.message.text, update.message.from_user.username))
     return 'OK'
 def getSum(query, userName):
