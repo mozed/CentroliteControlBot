de1a756dcfafc1beff9908a0bbae795c3655d3b3
mozed <32892272+mozed@users.noreply.github.com>
2017-10-19 08:44:14 +0300

    Update app.py

--- app.py
+++ app.py
@@ -8,6 +8,16 @@ import sys
 
 # routes contains the HTTP handlers for our server and must be imported.
 import routes
+import telebot
+
+TOKEN = 'AAFvOi0o7SbiuNrNk-T4rWD6McEtyQVUixQ'
+APPNAME = 'CentCTB'
+
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(content_types=["text"])
+def repeat_all_messages(message): # Название функции не играет никакой роли, в принципе
+    bot.send_message(message.chat.id, message.text)
 
 class SSLWSGIRefServer(bottle.ServerAdapter):
     def run(self, handler):
@@ -36,6 +46,7 @@ if __name__ == '__main__':
     app = bottle.Bottle()
     srv = SSLWSGIRefServer(host="centctb.azurewebsites.net", port=80)
     bottle.run(server=srv)
+    bot.polling(none_stop=True)
 
     @bottle.route('/static/<filepath:path>')
     def server_static(filepath):
