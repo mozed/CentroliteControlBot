76f7a77a44681f39b1a428d18c16cc2823c731e1
mozed <32892272+mozed@users.noreply.github.com>
2017-10-19 08:33:23 +0300

    Update app.py

--- app.py
+++ app.py
@@ -33,13 +33,9 @@ def wsgi_app():
     return bottle.default_app()
 
 if __name__ == '__main__':
-    PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
-    STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static').replace('\\', '/')
-    HOST = os.environ.get('SERVER_HOST', 'localhost')
-    try:
-        PORT = int(os.environ.get('SERVER_PORT', '5555'))
-    except ValueError:
-        PORT = 5555
+    app = bottle.Bottle()
+    srv = SSLWSGIRefServer(host="centctb.azurewebsites.net", port=80)
+    bottle.run(server=srv)
 
     @bottle.route('/static/<filepath:path>')
     def server_static(filepath):
